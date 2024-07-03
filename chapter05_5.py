99a7d38503b0e38e65cee44031d691eecc24f8a6
Ilya A. Pimenov <ilya.pimenov@gmail.com>
2024-07-03 17:02:49 +0200

    Code along from finishing 6.5

--- chapter05_5.py
+++ chapter05_5.py
@@ -4,58 +4,13 @@ from chapter04 import GPTModel
 from chapter05 import GPT_CONFIG_124M, generate, text_to_token_ids, token_ids_to_text, calc_loss_loader
 import torch
 import tiktoken
-
-# Load file that has logic to download GPT-2 weights in Tensor Flow format
-url = (
-        "https://raw.githubusercontent.com/rasbt/"
-        "LLMs-from-scratch/main/ch05/"
-        "01_main-chapter-code/gpt_download.py"
-)
-filename = url.split('/')[-1]
-urllib.request.urlretrieve(url, filename)
-
-from gpt_download import download_and_load_gpt2
-settings, params = download_and_load_gpt2(
-    model_size="124M", models_dir="gpt2"
-)
-
-print("Settings:", settings)
-print("Parameter dictionary keys:", params.keys())
-
-print(params["wte"])
-print("Token embedding weight tensor dimensions:", params["wte"].shape)
-
-model_configs = {
-        "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
-        "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
-        "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
-        "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
-        }
-
-model_name = "gpt2-small (124M)"
-NEW_CONFIG = GPT_CONFIG_124M.copy()
-NEW_CONFIG.update(model_configs[model_name])
-
-NEW_CONFIG.update({"context_length": 1024})
-
-# Biases are not used anymore as they don't seem to improve modeling performance, need to update it for consistency
-NEW_CONFIG.update({"qkv_bias": True})
-
-# device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-device = torch.device("cpu")
-tokenizer = tiktoken.get_encoding("gpt2")
-
-# Now with update config we can init our model
-gpt = GPTModel(NEW_CONFIG)
-gpt.eval()
+import numpy as np
 
 def assign(left, right):
     if left.shape != right.shape:
         raise ValueError(f"Shape mismatch. Left: {left.shape}, "
                           "Right: {right.shape}")
     return torch.nn.Parameter(torch.tensor(right))
-
-import numpy as np
 
 # This implementation is mainly a lot of guess work on behalf of author of the book
 def load_weights_into_gpt(gpt, params):
@@ -92,58 +47,102 @@ def load_weights_into_gpt(gpt, params):
     gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
     gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
 
+def main():
+    # Load file that has logic to download GPT-2 weights in Tensor Flow format
+    url = (
+            "https://raw.githubusercontent.com/rasbt/"
+            "LLMs-from-scratch/main/ch05/"
+            "01_main-chapter-code/gpt_download.py"
+    )
+    filename = url.split('/')[-1]
+    urllib.request.urlretrieve(url, filename)
+    
+    from gpt_download import download_and_load_gpt2
+    settings, params = download_and_load_gpt2(
+        model_size="124M", models_dir="gpt2"
+    )
+    
+    print("Settings:", settings)
+    print("Parameter dictionary keys:", params.keys())
+    
+    print(params["wte"])
+    print("Token embedding weight tensor dimensions:", params["wte"].shape)
+    
+    model_configs = {
+            "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+            "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+            "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+            "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+            }
+    
+    model_name = "gpt2-small (124M)"
+    NEW_CONFIG = GPT_CONFIG_124M.copy()
+    NEW_CONFIG.update(model_configs[model_name])
+    
+    NEW_CONFIG.update({"context_length": 1024})
+    
+    # Biases are not used anymore as they don't seem to improve modeling performance, need to update it for consistency
+    NEW_CONFIG.update({"qkv_bias": True})
+    
+    # device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    device = torch.device("cpu")
+    tokenizer = tiktoken.get_encoding("gpt2")
+    
+    # Now with update config we can init our model
+    gpt = GPTModel(NEW_CONFIG)
+    gpt.eval()
 
-# Let's try to load it now
-load_weights_into_gpt(gpt, params)
-gpt.to(device)
-
-torch.manual_seed(123)
-token_ids = generate(
-        model=gpt,
-        idx=text_to_token_ids("Every effort moves you", tokenizer).to(device),
-        max_new_tokens=25,
-        context_size=NEW_CONFIG["context_length"],
-        top_k=50,
-        temperature=1.5
-)
-print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
-
-# Exercise 5.5 Losses as measured on the verdict
-
-# Load the verdict short story
-file_path = "the-verdict.txt"
-with open(file_path, "r", encoding="utf-8") as file:
-    text_data = file.read()
-
-train_ratio = 0.50
-split_idx = int(train_ratio *len(text_data))
-train_data = text_data[:split_idx]
-val_data = text_data[split_idx:]
-
-torch.manual_seed(123)
-
-train_loader = create_dataloader_v1(
-        train_data,
-        batch_size=2,
-        max_length=NEW_CONFIG["context_length"],
-        stride=NEW_CONFIG["context_length"],
-        drop_last=True,
-        shuffle=True,
-        num_workers=0
-)
-val_loader = create_dataloader_v1(
-        val_data,
-        batch_size=2, # A more common batch size would be 1024, this is just for the demonstration purpose
-        max_length=NEW_CONFIG["context_length"],
-        stride=NEW_CONFIG["context_length"],
-        drop_last=False,
-        shuffle=False,
-        num_workers=0
-)
-
-with torch.no_grad():
-    train_loss = calc_loss_loader(train_loader, gpt, device)
-    val_loss = calc_loss_loader(val_loader, gpt, device)
-
-print("Train lossess:\n", train_loss)
-print("Validation losses:\n", val_loss)
+    # Let's try to load it now
+    load_weights_into_gpt(gpt, params)
+    gpt.to(device)
+    
+    torch.manual_seed(123)
+    token_ids = generate(
+            model=gpt,
+            idx=text_to_token_ids("Every effort moves you", tokenizer).to(device),
+            max_new_tokens=25,
+            context_size=NEW_CONFIG["context_length"],
+            top_k=50,
+            temperature=1.5
+    )
+    print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+    
+    # Exercise 5.5 Losses as measured on the verdict
+    
+    # Load the verdict short story
+    file_path = "the-verdict.txt"
+    with open(file_path, "r", encoding="utf-8") as file:
+        text_data = file.read()
+    
+    train_ratio = 0.50
+    split_idx = int(train_ratio *len(text_data))
+    train_data = text_data[:split_idx]
+    val_data = text_data[split_idx:]
+    
+    torch.manual_seed(123)
+    
+    train_loader = create_dataloader_v1(
+            train_data,
+            batch_size=2,
+            max_length=NEW_CONFIG["context_length"],
+            stride=NEW_CONFIG["context_length"],
+            drop_last=True,
+            shuffle=True,
+            num_workers=0
+    )
+    val_loader = create_dataloader_v1(
+            val_data,
+            batch_size=2, # A more common batch size would be 1024, this is just for the demonstration purpose
+            max_length=NEW_CONFIG["context_length"],
+            stride=NEW_CONFIG["context_length"],
+            drop_last=False,
+            shuffle=False,
+            num_workers=0
+    )
+    
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, gpt, device)
+        val_loss = calc_loss_loader(val_loader, gpt, device)
+    
+    print("Train lossess:\n", train_loss)
+    print("Validation losses:\n", val_loss)
--- chapter06.py
+++ chapter06.py
@@ -7,6 +7,11 @@ import tiktoken
 import torch
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+from gpt_download import download_and_load_gpt2
+from chapter05 import GPTModel
+from chapter05_5 import load_weights_into_gpt
+from chapter04 import generate_text_simple
+from chapter05 import text_to_token_ids, token_ids_to_text
 
 url = "http://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
 zip_path = "sms_spam_collection.zip"
@@ -169,6 +174,61 @@ def main():
     print(f"{len(train_loader)} training batches")
     print(f"{len(val_loader)} validation batches")
     print(f"{len(test_loader)} test batches")
+
+    # 6.4 Initialize a model with pretrained weights
+    CHOOSE_MODEL = "gpt2-small (124M)"
+    INPUT_PROMPT = "Every effort moves"
+    BASE_CONFIG = {
+            "vocab_size": 50257,     # Vocabulary size
+            "context_length": 1024,  # Context length
+            "drop_rate": 0.0,        # Dropout rate
+            "qkv_bias": True         # Query-key-value bias
+    }
+    model_configs = {
+            "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+            "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+            "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+            "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+    }
+    BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+
+    assert train_dataset.max_length <= BASE_CONFIG["context_length"], (
+            f"Dataset length {train_dataset.max_length} exceeds model's context "
+            f"length {BASE_CONFIG['context_length']}. Reinitialize data sets with "
+            f"`max_length={BASE_CONFIG['context_length']}`"
+    )
+
+    model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
+    settings, params = download_and_load_gpt2(model_size=model_size, models_dir="gpt2")
+    
+    model = GPTModel(BASE_CONFIG)
+    load_weights_into_gpt(model, params)
+    model.eval()
+    
+    # Model is loaded, lets ensure it generate coherent text
+    text_1 = "Every effort moves you"
+    token_ids = generate_text_simple(
+            model=model,
+            idx=text_to_token_ids(text_1, tokenizer),
+            max_new_tokens=15,
+            context_size=BASE_CONFIG["context_length"]
+    )
+    print(token_ids_to_text(token_ids, tokenizer))
+
+    # Lets see if the model can classify spam messages by prompting it with instructions
+    text_2 = (
+            "Is the following text 'spam'? Answer with 'yes' or 'no':"
+            " 'You are a winner you have been specially"
+            " selected to receive $1000 cash or a $2000 award.'"
+    )
+    token_ids = generate_text_simple(
+            model=model,
+            idx=text_to_token_ids(text_2, tokenizer),
+            max_new_tokens=23,
+            context_size=BASE_CONFIG["context_length"]
+    )
+    print(token_ids_to_text(token_ids, tokenizer))
+
 
 
 if __name__ == "__main__":
