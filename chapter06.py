de49d51ab5b6815fd7c28b820337c8d1ffa110c9
Ilya A. Pimenov <ilya.pimenov@gmail.com>
2024-07-05 13:24:10 +0200

    Chapter 6.7 — trained and plotted results of a classifier.

--- chapter06.py
+++ chapter06.py
@@ -5,9 +5,11 @@ from pathlib import Path
 import pandas as pd
 import tiktoken
 import torch
+import time
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from gpt_download import download_and_load_gpt2
+import matplotlib.pyplot as plt
 from chapter05 import GPTModel
 from chapter05_5 import load_weights_into_gpt
 from chapter04 import generate_text_simple
@@ -135,6 +137,67 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
         loss = calc_loss_batch(input_batch, target_batch, model, device)
         total_loss += loss.item()
     return total_loss / num_batches
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
+
+
+def train_classifier_simple(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter, tokenizer):
+    # Initialize lists to track losses and examples seen
+    train_losses, val_losses, train_accs, val_accs = [], [] , [], []
+    examples_seen, global_step = 0, -1
+
+    # Main training loop
+    for epoch in range(num_epochs):
+        model.train()
+
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            examples_seen += input_batch.shape[0]
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, Val loss {val_loss:.3f}")
+        train_accuracy = calc_accuracy_loader(
+                train_loader, model, device, num_batches=eval_iter
+        )
+        val_accuracy = calc_accuracy_loader(
+                val_loader, model, device, num_batches=eval_iter
+        )
+        print(f"Training accuracy: {train_accuracy*100:.2f}% | ", end="")
+        print(f"Validation accuracy: {val_accuracy*100:.2f}%")
+        train_accs.append(train_accuracy)
+        val_accs.append(val_accuracy)
+    return train_losses, val_losses, train_accs, val_accs, examples_seen
+
+def plot_values(epochs_seen, examples_seen, train_values, val_values, label="loss"):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+
+    ax1.plot(epochs_seen, train_values, label=f"Training {label}")
+    ax1.plot(epochs_seen, val_values, linestyle="-.", label=f"Validation {label}")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel(label.capitalize())
+    ax1.legend()
+
+    ax2 = ax1.twiny()
+    ax2.plot(examples_seen, train_values, alpha=0) # Invinsible
+    ax2.set_xlabel("Examples seen")
+
+    fig.tight_layout()
+    plt.savefig(f"{label}-plot.pdf")
+    plt.show()
 
 
 def main():
@@ -312,7 +375,7 @@ def main():
     print("Class label:", label.item())
 
     # Test out classification accuracies
-    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    device = torch.device("cpu") # torch.device("mps" if torch.backends.mps.is_available() else "cpu")
     model.to(device)
 
     torch.manual_seed(123)
@@ -334,7 +397,39 @@ def main():
     print(f"Validation loss: {val_loss:.3f}")
     print(f"Test loss: {test_loss:.3f}")
 
+    # Time to train! 6.7
+    start_time = time.time()
+    torch.manual_seed(123)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5, weight_decay=0.1)
+    num_epochs = 5
 
+    train_losses, val_losses, train_accs, val_accs, examples_seen = train_classifier_simple(
+            model, train_loader, val_loader, optimizer, device,
+            num_epochs=num_epochs, eval_freq=50, eval_iter=5,
+            tokenizer=tokenizer
+    )
+    end_time = time.time()
+    execution_time_minutes = (end_time - start_time) / 60
+    print(f"Training completed in {execution_time_minutes:.2f} minutes.")
+
+    # Plotting what we got
+    epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+    examples_seen_tensor = torch.linspace(0, examples_seen, len(train_losses))
+
+    #plot_values(epochs_tensor, examples_seen_tensor, train_losses, val_losses)
+
+    epochs_tensor = torch.linspace(0, num_epochs, len(train_accs))
+    examples_seen_tensor = torch.linspace(0, examples_seen, len(train_accs))
+    #plot_values(epochs_tensor, examples_seen_tensor, train_accs, val_accs, label="accuracy")
+
+    # Now evaluate accuracy for the entire dataset
+    train_accuracy = calc_accuracy_loader(train_loader, model, device)
+    val_accuracy = calc_accuracy_loader(val_loader, model, device)
+    test_accuracy = calc_accuracy_loader(test_loader, model, device)
+
+    print(f"Training accuracy: {train_accuracy*100:.2f}%")
+    print(f"Validation accuracy: {val_accuracy*100:.2f}%")
+    print(f"Test accuracy: {test_accuracy*100:.2f}%")
 
 
 
