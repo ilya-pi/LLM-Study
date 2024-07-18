912e18388d2ac06fcf4ab1cb4d7a921aa0a7bd36
Ilya A. Pimenov <ilya.pimenov@gmail.com>
2024-07-18 12:30:38 +0200

    Two more sub-chapters of chapter 7.

--- chapter07.py
+++ chapter07.py
@@ -1,6 +1,15 @@
 import json
 import os
 import urllib.request
+import torch
+from torch.utils.data import Dataset
+import tiktoken
+from functools import partial
+from torch.utils.data import DataLoader
+from gpt_download import download_and_load_gpt2
+from chapter04 import GPTModel
+from chapter05_5 import load_weights_into_gpt
+from chapter05 import generate, text_to_token_ids, token_ids_to_text
 
 def download_and_load_file(file_path, url):
     if not os.path.exists(file_path):
@@ -23,6 +32,102 @@ def format_input(entry):
             )
     input_text = f"\n\n### Input:\n{entry['input']}" if entry["input"] else ""
     return instruction_text + input_text
+
+class InstructionDataset(Dataset):
+    def __init__(self, data, tokenizer):
+        self.data = data
+        self.encoded_texts = []
+        for entry in data:
+            instruction_plus_input = format_input(entry)
+            response_text = f"\n\n### Response:\n{entry['output']}"
+            full_text = instruction_plus_input + response_text
+            self.encoded_texts.append(
+                    tokenizer.encode(full_text)
+            )
+
+    def __getitem__(self, index):
+        return self.encoded_texts[index]
+
+    def __len__(self):
+        return len(self.data)
+
+def custom_collate_draft_1(
+        batch,
+        pad_token_id=50256,
+        device="cpu"
+):
+    batch_max_length = max(len(item) for item in batch)
+    inputs_lst = []
+
+    for item in batch:
+        new_item = item.copy()
+        #new_item += [pad_token_id]
+
+        padded = new_item + [pad_token_id] * (batch_max_length - len(new_item))
+
+        #inputs = torch.tensor(padded[:-1])
+        inputs = torch.tensor(padded)
+        inputs_lst.append(inputs)
+
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    return inputs_tensor
+
+def custom_collate_draft_2(
+        batch,
+        pad_token_id=50256,
+        device="cpu"
+):
+    batch_max_length = max(len(item)+1 for item in batch)
+    inputs_lst, targets_lst = [], []
+
+    for item in batch:
+        new_item = item.copy()
+        new_item += [pad_token_id]
+        padded = new_item + [pad_token_id] * (batch_max_length - len(new_item))
+        inputs = torch.tensor(padded[:-1])
+        targets = torch.tensor(padded[1:])
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+    return inputs_tensor, targets_tensor
+
+def custom_collate_fn(
+        batch,
+        pad_token_id=50256,
+        ignore_index=-100,
+        allowed_max_length=None,
+        device="cpu"
+):
+    batch_max_length = max(len(item)+1 for item in batch)
+    inputs_lst, targets_lst = [], []
+
+    for item in batch:
+        new_item = item.copy()
+        new_item += [pad_token_id]
+        # Pad sequences to max_length
+        padded = new_item + [pad_token_id] * (batch_max_length - len(new_item))
+        inputs = torch.tensor(padded[:-1]) # Truncate the last token for inputs
+        targets = torch.tensor(padded[1:]) # Shift +1 to the right for targets
+
+        mask = targets == pad_token_id
+        indices = torch.nonzero(mask).squeeze()
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets = targets[:allowed_max_length]
+
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+    return inputs_tensor, targets_tensor
+
+
 
 file_path = "instruction-data.json"
 url = "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch07/01_main-chapter-code/instruction-data.json"
@@ -52,3 +157,145 @@ val_data = data[train_portion + test_portion:]
 print("Training set length:", len(train_data))
 print("Validation set length:", len(val_data))
 print("Test set length:", len(test_data))
+
+tokenizer = tiktoken.get_encoding("gpt2")
+print(tokenizer.encode("<|endoftext|>", allowed_special={"<|endoftext|>"}))
+# Output is [50256]
+
+# Trying our custom collate
+inputs_1 = [0, 1, 2, 3, 4]
+inputs_2 = [5, 6]
+inputs_3 = [7, 8, 9]
+batch = (
+    inputs_1,
+    inputs_2,
+    inputs_3
+)
+print(custom_collate_draft_1(batch))
+
+# Adding targets
+inputs, targets = custom_collate_draft_2(batch)
+print(inputs)
+print(targets)
+
+# Trying version #3 of custom collate function
+inputs, targets = custom_collate_fn(batch)
+print(inputs)
+print(targets)
+
+# Explanation about -100
+logits_1 = torch.tensor(
+    [[-1.0, 1.0], # prediction for 1st token
+     [-0.5, 1.5]] # prediction for 2nd token
+)
+targets_1 = torch.tensor([0, 1])
+loss_1 = torch.nn.functional.cross_entropy(logits_1, targets_1)
+print(loss_1)
+
+logits_2 = torch.tensor(
+    [[-1.0, 1.0], # prediction for 1st token
+     [-0.5, 1.5], # prediction for 2nd token
+     [-0.5, 1.5]] # prediction for 3rd token
+)
+targets_2 = torch.tensor([0, 1, 1])
+loss_2 = torch.nn.functional.cross_entropy(logits_2, targets_2)
+print(loss_2)
+
+targets_3 = torch.tensor([0, 1, -100])
+loss_3 = torch.nn.functional.cross_entropy(logits_2, targets_3)
+print(loss_3)
+print("loss_1 == loss_3:", loss_1 == loss_3)
+
+# 7.4
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# if torch.backends.mps.is_available():
+#    device = torch.device("mps")
+print("Device:", device)
+
+customized_collate_fn = partial(custom_collate_fn, device=device, allowed_max_length=1024)
+
+num_workers = 0
+batch_size = 8
+
+torch.manual_seed(123)
+
+train_dataset = InstructionDataset(train_data, tokenizer)
+train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        collate_fn=customized_collate_fn,
+        shuffle=True,
+        drop_last=True,
+        num_workers=num_workers
+)
+
+val_dataset = InstructionDataset(val_data, tokenizer)
+val_loader = DataLoader(
+    val_dataset,
+    batch_size=batch_size,
+    collate_fn=customized_collate_fn,
+    shuffle=False,
+    drop_last=False,
+    num_workers=num_workers
+)
+
+test_dataset = InstructionDataset(test_data, tokenizer)
+test_loader = DataLoader(
+    test_dataset,
+    batch_size=batch_size,
+    collate_fn=customized_collate_fn,
+    shuffle=False,
+    drop_last=False,
+    num_workers=num_workers
+)
+
+# Dimensions of the input and target batches generated by the training loader
+
+print("Train loader:")
+for inputs, targets in train_loader:
+    print(inputs.shape, targets.shape)
+
+# 7.5
+
+BASE_CONFIG = {
+        "vocab_size": 50257,      # Vocabulary size
+        "context_length": 1024,   # Context length
+        "drop_rate": 0.0,         # Dropout rate
+        "qkv_bias": True          # Query-key-value bias
+}
+
+model_configs = {
+        "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+        "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+        "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+        "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+}
+
+CHOOSE_MODEL = "gpt2-medium (355M)"
+BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+
+model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
+settings, params = download_and_load_gpt2(model_size=model_size, models_dir="gpt2")
+
+model = GPTModel(BASE_CONFIG)
+load_weights_into_gpt(model, params)
+model.eval()
+
+# Trying a sample question without fine-tuning
+
+torch.manual_seed(123)
+input_text = format_input(val_data[0])
+print(input_text)
+token_ids = generate(
+        model=model,
+        idx=text_to_token_ids(input_text, tokenizer),
+        max_new_tokens=35,
+        context_size=BASE_CONFIG["context_length"],
+        eos_id=50256,
+)
+generated_text = token_ids_to_text(token_ids, tokenizer)
+
+response_text = generated_text[len(input_text):].strip()
+print("\n\n-----OUTPUT-----\n\n")
+print(response_text)
